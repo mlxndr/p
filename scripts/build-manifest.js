18cb8299b6bab174681ee060ef5a359da3d85b64
Marc Alexander <m@mga.is>
2026-01-03 04:16:07 +0000

    CV and removing templates from the list

--- scripts/build-manifest.js
+++ scripts/build-manifest.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const rootDir = '.';
-const excludedFolders = ['img', 'gamma', 'inc', 'f', 'css', 'archive', '.git', '.github', 'scripts', 'node_modules'];
+const excludedFolders = ['img', 'gamma', 'inc', 'f', 'css', 'archive', '.git', '.github', 'scripts', 'templates', 'node_modules'];
 
 const presentations = fs.readdirSync(rootDir, { withFileTypes: true })
     .filter(entry => entry.isDirectory() && !excludedFolders.includes(entry.name.toLowerCase()))
